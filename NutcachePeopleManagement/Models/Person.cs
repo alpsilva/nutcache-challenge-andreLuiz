e2bd096054211f4c313005dafa28a9e9c923b5ad
André Luiz <alps2@cin.ufpe.br>
2021-09-05 12:18:57 -0300

    Added an Id attribute to Person class

--- NutcachePeopleManagement/Models/Person.cs
+++ NutcachePeopleManagement/Models/Person.cs
@@ -7,6 +7,7 @@ namespace NutcachePeopleManagement.Models
 {
     public class Person
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public string Gender { get; set; }
