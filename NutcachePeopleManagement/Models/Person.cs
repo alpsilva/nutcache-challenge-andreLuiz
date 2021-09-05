b2dca27eee90ed92ead238d2f07710297668ca4a
André Luiz <alps2@cin.ufpe.br>
2021-09-05 10:39:13 -0300

    Added Person model

---
+++ NutcachePeopleManagement/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutcachePeopleManagement.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime StartDate { get; set; }
+        // Can be: Mobile, Fronted, Backend, null
+        public string Team { get; set; }
+    }
+}
