f34cf8cefb782f3f951aa5d00da58de3991b3377
André Luiz <alps2@cin.ufpe.br>
2021-09-05 11:17:25 -0300

    Added PersonContext as inMemory database

---
+++ NutcachePeopleManagement/Models/PersonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NutcachePeopleManagement.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- NutcachePeopleManagement/Startup.cs
+++ NutcachePeopleManagement/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NutcachePeopleManagement.Models;
 
 namespace NutcachePeopleManagement
 {
@@ -27,6 +22,9 @@ namespace NutcachePeopleManagement
         {
 
             services.AddControllers();
+
+            services.AddDbContext<PersonContext>(opt =>
+                                               opt.UseInMemoryDatabase("PeopleList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
